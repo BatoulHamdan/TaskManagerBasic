4b5e3e98713abe04b8351b8ce6175c25ee143098
Batoul Hamdan <batoul.hamdan19@gmail.com>
2023-11-07 22:41:47 +0200

    Adding edit button

--- script.js
+++ script.js
@@ -17,16 +17,33 @@ function add_task() {
         let new_li = document.createElement('li')
         new_li.innerText = new_task.value
 
+        // Create edit button
+        let edit_btn = document.createElement('button')
+        edit_btn.innerHTML = '&#128394;'
+        edit_btn.setAttribute('class', 'task_btn')
+
+        //Edit task function
+        edit_btn.addEventListener('click', function() {
+            let edit_task = prompt("Edit Task: ", new_li.innerText)
+            if(edit_task !== null) {
+                new_li.innerText = edit_task
+                new_li.appendChild(edit_btn)
+                new_li.appendChild(delete_btn)
+                todo_list.appendChild(new_li)
+            }
+        })
+
         // Create delete button
         let delete_btn = document.createElement('button')
         delete_btn.innerHTML = '&#10005;'
-        delete_btn.setAttribute('class', 'delete-task')
+        delete_btn.setAttribute('class', 'task_btn')
 
         // Delete task function
         delete_btn.addEventListener('click', function() {
             new_li.remove()
         })
 
+        new_li.appendChild(edit_btn)
         new_li.appendChild(delete_btn)
         todo_list.appendChild(new_li)
 

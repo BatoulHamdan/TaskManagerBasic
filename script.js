3b3374e31a3f50cc27fb3483a42335322005c685
Batoul Hamdan <batoul.hamdan19@gmail.com>
2023-11-08 22:00:30 +0200

    Updating add_task function

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Defining Lists
 let todo_list = document.getElementById('todo-list')
-let doing_list = document.getElementById('doing-list')
 
 // Defining Add button
 let add_btn = document.getElementById('add-task');
@@ -18,58 +17,49 @@ function add_task() {
     else {
         // Add new task to the todo list
         let new_li = document.createElement('li')
-        new_li.innerText = new_task.value
 
         // Create task div
         let task_div = document.createElement('div')
         task_div.className = 'task-div'
+
+        // Create task container div
+        let task = document.createElement('div')
+        task.className = 'task'
+        task.innerText = new_task.value
 
         // Create checkbox
         let task_checkbox = document.createElement('input');
         task_checkbox.type = 'checkbox';
         task_checkbox.setAttribute('class', 'task-checkbox');
 
-        // Create add button
-        let add_btn = document.getElementById('add-task')
-        add_btn.addEventListener('click', add_task)
-
         // Create edit button
         let edit_btn = document.createElement('button')
         edit_btn.innerHTML = '&#128394;'
-        edit_btn.setAttribute('class', 'task_btn')
 
         //Edit task function
         edit_btn.addEventListener('click', function() {
-            let edit_task = prompt("Edit Task: ", new_li.innerText)
+            let edit_task = prompt("Edit Task: ", task.innerText)
             if(edit_task !== null) {
-                new_li.innerText = edit_task
-
-                task_div.appendChild(edit_btn)
-                task_div.appendChild(delete_btn)
-                todo_list.appendChild(task_div)
+                task.innerText = edit_task
             }
         })
 
         // Create delete button
         let delete_btn = document.createElement('button')
         delete_btn.innerHTML = '&#10005;'
-        delete_btn.setAttribute('class', 'task_btn')
 
         // Delete task function
         delete_btn.addEventListener('click', function() {
-            new_li.remove()
+            task_div.remove()
         })
 
         task_div.appendChild(task_checkbox)
-        task_div.appendChild(new_li)
+        task_div.appendChild(task)
         task_div.appendChild(edit_btn)
         task_div.appendChild(delete_btn)
-
-        // new_li.addEventListener('click', function() {
-        //     doing_list.appendChild(task_div)
-        // })
         
-        todo_list.appendChild(task_div)
+        new_li.appendChild(task_div)
+        todo_list.appendChild(new_li)
 
         new_task.value = ''
     }
